4154bc5acdf050efadb74e8367dfa43ad5493d36
Danny Tuppeny <danny@tuppeny.com>
2019-10-16 13:15:11 +0100

    Update test_runner.ts

--- src/test/test_runner.ts
+++ src/test/test_runner.ts
@@ -3,19 +3,12 @@ console.log("Starting test runner...");
 import * as glob from "glob";
 import * as Mocha from "mocha";
 import * as path from "path";
-import { MultiReporter } from "./mocha_multi_reporter";
 
 module.exports = {
 	run(testsRoot: string, cb: (error: any, failures?: number) => void): void {
 		// Create the mocha test
 		const mocha = new Mocha({
 			forbidOnly: !!process.env.MOCHA_FORBID_ONLY,
-			reporter: MultiReporter,
-			reporterOptions: {
-				output: process.env.TEST_XML_OUTPUT,
-				summaryFile: process.env.TEST_CSV_SUMMARY,
-				testRunName: process.env.TEST_RUN_NAME,
-			},
 			slow: 10000,       // increased threshold before marking a test as slow
 			timeout: 180000,   // increased timeout because starting up Code, Analyzer, Pub, etc. is slooow
 			ui: "bdd",        // the TDD UI is being used in extension.test.ts (suite, test, etc.)
