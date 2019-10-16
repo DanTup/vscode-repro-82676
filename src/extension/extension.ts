b27e790728cbf29be73b5b06a74ab8b21ebcb688
Danny Tuppeny <danny@tuppeny.com>
2019-10-16 12:55:54 +0100

    Remove all other extension code

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,39 +1,13 @@
-import * as path from "path";
 import * as vs from "vscode";
-import { analyzerSnapshotPath, dartVMPath } from "../shared/constants";
-import { DartWorkspaceContext } from "../shared/interfaces";
-import { EmittingLogger } from "../shared/logging";
-import { Analyzer } from "./analysis/analyzer";
-import { config } from "./config";
-import { SdkUtils } from "./sdk/utils";
 
 export const FLUTTER_SUPPORTS_ATTACH = "dart-code:flutterSupportsAttach";
 export const SERVICE_EXTENSION_CONTEXT_PREFIX = "dart-code:serviceExtension.";
 export const SERVICE_CONTEXT_PREFIX = "dart-code:service.";
 
-let analyzer: Analyzer;
-
-// TODO: If dev mode, subscribe to logs for errors/warnings and surface to UI
-// (with dispose calls)
-const logger = new EmittingLogger();
-
 export async function activate(context: vs.ExtensionContext, isRestart: boolean = false) {
 	console.log("Activating extension!");
 
-	const sdkUtils = new SdkUtils(logger);
-	const workspaceContextUnverified = await sdkUtils.scanWorkspace();
-
-	const workspaceContext = workspaceContextUnverified as DartWorkspaceContext;
-	const sdks = workspaceContext.sdks;
-
-	// Fire up the analyzer process.
-	const analyzerPath = config.analyzerPath || path.join(sdks.dart, analyzerSnapshotPath);
-
-	analyzer = new Analyzer(logger, path.join(sdks.dart, dartVMPath), analyzerPath);
-	context.subscriptions.push(analyzer);
-
 	console.log("Has finished activating extension!");
-
 }
 
 export async function deactivate(isRestart: boolean = false): Promise<void> {
